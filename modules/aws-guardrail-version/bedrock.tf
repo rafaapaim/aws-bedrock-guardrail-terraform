e7be9d3a48870f3af5731281972d03a0baed4846
Rafael <r_paim@outlook.com>
2025-07-06 11:40:35 -0300

    fix some modules and the project structure

--- modules/aws-guardrail-version/bedrock.tf
+++ modules/aws-guardrail-version/bedrock.tf
@@ -1,4 +1,4 @@
-resource "aws_bedrock_guardrail_version" "guardrail_new_version" {
+resource "aws_bedrock_guardrail_version" "guardrail_version" {
   guardrail_arn = var.guardrail_arn
   description   = var.description
   skip_destroy  = var.skip_destroy
